d84085d33ba25af847bcd256c99c206b111983d1
majidazarasa <46866429+majidazarasa@users.noreply.github.com>
2019-04-09 21:06:06 +0430

    Add files via upload

---
+++ Session-20/js/model.js
@@ -0,0 +1,17 @@
+// let firstName, lastName;
+// firstName="Majid";
+// lastName="Azarasa"
+// function fullName()
+// {
+//     var fname=firstName+" "+lastName;
+//     return fname;
+// }
+// function fullName(firstName, lastName)
+// {
+//     var fname=firstName+" "+lastName;
+//     return fname;
+// }
+function fullName(firstName="Mr.", lastName)
+{
+    return firstName+" "+lastName;
+}
